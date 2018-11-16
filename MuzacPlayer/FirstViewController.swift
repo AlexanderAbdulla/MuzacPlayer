25c0ca21a1c7f6cffe8d24dff945e3455e255e8f
Alex Abdulla <alexander.amadeus.abdulla@gmail.com>
2018-11-16 10:49:47 -0800

    Initial Commit

---
+++ MuzacPlayer/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MuzacPlayer
+//
+//  Created by Alex Abdulla on 2018-11-16.
+//  Copyright © 2018 Alex Abdulla. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MuzacPlayer/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MuzacPlayer
+//
+//  Created by Alex Abdulla on 2018-11-16.
+//  Copyright © 2018 Alex Abdulla. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
